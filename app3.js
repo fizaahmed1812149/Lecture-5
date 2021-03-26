9a273f0fe74ddc5e56cb3678e88ee618f85fa44a
Fiza Ahmed <81019245+fizaahmed1812149@users.noreply.github.com>
2021-03-27 01:00:10 +0500

    Update app3.js

--- app3.js
+++ app3.js
@@ -4,10 +4,10 @@ const product=require('./routes/product.route');
 const app=express();
 mongoose.connect("mongodb://localhost/productsDb",{useNewUrlParser: true, useUnifiedTopology: true})
 .then(()=>{
-    console.log('Successfully connected to the database')
+    console.log('We have successfully connected to the database')
 })
 .catch(err => {
-    console.log("Cannot connect to the database!", err);
+    console.log("Sorry, cannot connect to the database!", err);
     process.exit();
 });
 app.use(bodyParser.json());
@@ -15,7 +15,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use('/products', product);
 let port = 1234;
 app.listen(port, () => {
-    console.log('Server is up and running on port number ' + port);
+    console.log('Server is up and running on the port number ' + port);
 });
 
-app.listen(3000,()=>{console.log('Server is up and running on port 3000')});
+app.listen(3000,()=>{console.log('Server is up and running on the port 3000')});
